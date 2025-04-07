804241bd72603b07edc0258f87747afb084ee503
lovepettersson <90205027+lovepettersson@users.noreply.github.com>
2025-04-07 09:38:36 +0200

    Update RepeaterEquations.py

--- RepeaterEquations.py
+++ RepeaterEquations.py
@@ -42,4 +42,19 @@ def generation_time(N, n, t_CZ, t_meas, t_gen):
     return t
 
 
+def gen_time_tree(b0, b1, b2, t_gen, t_CZ):
+    generation_time = b0 * (100 + b1 * (1 + b2)) * t_gen
+    control_phase_time = b0 * (3 + b1) * t_CZ
+    return generation_time + control_phase_time
+
+
+
+
+def generation_time_RGS(b_0, b_1, t_CZ, t_meas, t_gen, N):
+    gen_t = (1 + b_0 * b_1) * t_gen
+    CZ_t = (2 + b_0) * t_CZ
+    meas_E_t = (b_0 + 2) * t_meas
+    t = N * (gen_t + CZ_t + meas_E_t) + t_meas
+    return t
+
 
